85958b6db91a6e3bb2d8face9220884f86fd21de
r2872 <84784400+r2872@users.noreply.github.com>
2021-09-02 16:36:57 +0900

    Initial commit

---
+++ app/src/main/java/com/example/phonebook_kotlin_0902/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.phonebook_kotlin_0902
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
