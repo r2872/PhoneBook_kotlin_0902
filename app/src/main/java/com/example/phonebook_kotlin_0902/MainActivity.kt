50bb4c75e5611fa944baebad46fcf3ce6e40db70
r2872 <84784400+r2872@users.noreply.github.com>
2021-09-03 11:43:46 +0900

    임시방편 -> 실제 파일에서 불러오기

--- app/src/main/java/com/example/phonebook_kotlin_0902/MainActivity.kt
+++ app/src/main/java/com/example/phonebook_kotlin_0902/MainActivity.kt
@@ -7,6 +7,9 @@ import com.example.phonebook_kotlin_0902.adapters.PhoneNumAdapter
 import com.example.phonebook_kotlin_0902.datas.PhoneNumData
 import kotlinx.android.synthetic.main.activity_edit_phone_num.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
 import java.text.SimpleDateFormat
 
 class MainActivity : BaseActivity() {
@@ -36,13 +39,13 @@ class MainActivity : BaseActivity() {
     override fun setValues() {
 
 //    임시방편: 직접 리스트에 데이터 객체 추가.
-        mPhoneNumList.add(PhoneNumData("테스트1", "010-1111-2222"))
-        mPhoneNumList.add(PhoneNumData("테스트2", "010-2222-2222"))
-        mPhoneNumList.add(PhoneNumData("테스트3", "010-3333-2222"))
-        mPhoneNumList.add(PhoneNumData("테스트4", "010-4444-2222"))
+//        mPhoneNumList.add(PhoneNumData("테스트1", "010-1111-2222"))
+//        mPhoneNumList.add(PhoneNumData("테스트2", "010-2222-2222"))
+//        mPhoneNumList.add(PhoneNumData("테스트3", "010-3333-2222"))
 
 //        수정방안 -> 파일을 불러와서, 그 내용을 읽고 -> PhoneNumDate() 로 변환 -> 목록에 추가.
 
+        readPhoneBookFromFile()
 //        어댑터 초기화.
         mAdapter = PhoneNumAdapter(mContext, R.layout.phone_num_list_item, mPhoneNumList)
 
@@ -58,6 +61,30 @@ class MainActivity : BaseActivity() {
 //            자바의 인터페이스를 개량한 버전의 코드.
 //            EditPhoneNumActivity 로 이동 : intent
             startActivity(Intent(mContext, EditPhoneNumActivity::class.java))
+        }
+    }
+
+    //    파일에서 폰번 읽어와서 -> 폰번목록에 추가.
+    fun readPhoneBookFromFile() {
+
+        val myFile = File(filesDir, "phoneBook.txt")
+
+        val fr = FileReader(myFile)
+        val br = BufferedReader(fr)
+
+        while (true) {
+
+//            읽어온 내용이 없다면 -> 종료. (null)
+            val line = br.readLine() ?: break
+
+            //            읽어온 line 을 => , 기준으로 분리
+            val infos = line.split(",")
+
+//            이름, 폰번만 우선 폰번데이터로.
+            val phoneNumData = PhoneNumData(infos[0], infos[1])
+
+//            만들어진 폰번 데이터 목록에 추가.
+            mPhoneNumList.add(phoneNumData)
         }
     }
 }
